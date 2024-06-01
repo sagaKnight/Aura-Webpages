8ca3b97bae8fae5349421e83dd6c118c3a712188
Rino Miguel Sagario Quijote <149220242+sagaKnight@users.noreply.github.com>
2024-06-01 18:18:02 +1000

    Broken Up Components for Homepage

---
+++ shadowsteel-forge/src/components/TheFaces.vue
@@ -0,0 +1,34 @@
+<template>
+  <!--Start of Staff Section + New Container -->
+  <div class="container border-bottom p-3 border-black">
+    <div class="row p-3 text-center">
+      <h3>The Faces Behind</h3>
+    </div>
+    <div class="row">
+      <div class="col-12 col-sm-6">
+        <img src="../assets/images/gouvea.png" alt="gouvea" class="profile rounded p-2">
+        <h3>Gouvea</h3>
+        <h4>Lead Designer. Marketer. Father.</h4>
+        <p>
+          Gouvea is our visionary weapon designer, blending artistry with
+          precision engineering. With a keen eye for innovation and safety, he
+          crafts weapons that redefine industry standards. With years of
+          expertise and a passion for pushing boundaries, Gouvea ensures our
+          arsenal remains at the forefront of technological advancement.
+        </p>
+      </div>
+      <div class="col-12 col-sm-6">
+        <img src="../assets/images/seetelo.png" alt="seetelo" class="profile rounded">
+        <h3>Seetelo</h3>
+        <h4>Lead Blacksmith. Learner. Explorer.</h4>
+        <p>
+          Seetelo, our esteemed lead blacksmith, embodies the artistry and
+          mastery of metallurgy. With a hammer as his scepter and fire as his
+          forge. His meticulous craftsmanship and unwavering dedication ensure
+          that every piece bearing his mark is not just a tool but a testament
+          to the enduring legacy of craftsmanship.
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
--- shadowsteel-forge/src/views/HomeView.vue
+++ shadowsteel-forge/src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup>
 import TheIntroduction from '../components/TheIntroduction.vue'
+import OnDisplay from '../components/OnDisplay.vue'
 </script>
 
 <template>
   <main>
     <TheIntroduction />
+    <OnDisplay />
   </main>
 </template>
