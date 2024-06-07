03d96b20001bbbeb64fb2ed180553ef177c2d283
Rino Miguel Sagario Quijote <149220242+sagaKnight@users.noreply.github.com>
2024-06-07 18:18:55 +1000

    Finished the solution. Finished Adriane Collection.

--- shadowsteel-forge/src/components/TheCollectionView.vue
+++ shadowsteel-forge/src/components/TheCollectionView.vue
@@ -1,11 +1,11 @@
 <template>
     <div>
-      <div v-if="collectionContent">
-        <div v-for="(content, index) in collectionContent" :key="index">
+      <div v-if="collectionJSON">
+        <div v-for="(content, index) in collectionJSON" :key="index">
           <div class="container text-center">
             <div class="row mt-3 d-flex justify-content-center align-items-center mb-1"> 
               <div class="col-8 col-xl-5 col-lg-6 col-md-7 col-sm-7">
-                <img :src="headerImageSrc(content)" alt="Header Image">
+                <img :src="getImageSrc(content, 'headerImage')" alt="Header Image" class="rounded">
               </div>
               <div class="col-10 col-xl-5 col-lg-6 col-md-4 col-sm-5">
                 <h1>{{ content.collectionHeading }}</h1>
@@ -21,11 +21,11 @@
               </div>
             </div>
             <div class="row d-flex justify-content-center">
-              <div class="col-8 col-xl-3 col-lg-3 col-md-6 col-sm-6">
-                <img class="rounded" src="../assets/images/collections/adriane-collection.jpg">
-                <figcaption>Name</figcaption>
-                <figcaption>700 USD</figcaption>
-              </div>
+                <div v-for="item in content.items" class="col-8 col-xl-3 col-lg-3 col-md-6 col-sm-6">
+                    <img class="rounded" :src="getImageSrc(item, 'itemImage')" alt="Item Image">
+                    <figcaption>{{ item.itemName }}</figcaption>
+                    <figcaption>{{ item.itemPrice }}</figcaption>
+                </div>
             </div>
           </div>
         </div>
@@ -37,21 +37,16 @@
   import data from '../components/data.json'
   
   export default {
-    data() {
-      return {
-        collectionsJson: data
-      }
-    },
     computed: {
-      collectionContent() {
-        return this.collectionsJson[this.$route.params.collection];
+      collectionJSON() {
+        return data[this.$route.params.collection];
       },
     },
     methods: {
     //Overly complicated solution to get images working here...
-      headerImageSrc(content) {
-        return new URL(`../assets/images/collections/adrianepage/${content.headerImage}`, import.meta.url).href;
-      }
+    getImageSrc(JSONobject, imageName) {
+        return new URL(`../assets/images/collections/adrianepage/${JSONobject[imageName]}`, import.meta.url).href;
+        }
     }
   }
   </script>
