fa1faecf3f92675d711fcb9d9534a49c3c335701
Rino Miguel Sagario Quijote <149220242+sagaKnight@users.noreply.github.com>
2024-10-18 13:12:33 +1100

    Update Header

--- Aura-Website/src/components/TheHomepageDisplay.vue
+++ Aura-Website/src/components/TheHomepageDisplay.vue
@@ -1,32 +1,17 @@
 <template>
-  <div class="container">
-    <div class="row px-5 mt-1 justify-content-center">
-      <div
-        class="col-12 col-xl-7 col-lg-10 col-md-10 col-sm-12 position-relative"
-      >
+  <div class="hero-container">
+    <div class="row mt-1 justify-content-center p-0">
         <RouterLink to="/collection/kelly">
-          <img
-            src="../assets/images/intro-header-image.jpg"
-            class="rounded"
-            alt="header"
-          />
+          <video src="../assets/videos/herovid-aura.mp4" autoplay muted loop class="hero-video"></video>
         </RouterLink>
-        <div class="position-absolute bottom-0 start-5 p-2 text-white">
+        <div class="position-absolute bottom-0 p-3 text-white">
           <h6 class="mb-0">Kelly Collection.</h6>
-        </div>
-      </div>
-      <div
-        class="col-8 col-xl-2 col-sm-5 align-self-center bg-white text-black p-4 rounded position-absolute top-20 start-20"
-      >
-        <h1>Aura</h1>
-        <h2>Relaxed. Effortless. Prepared.</h2>
-        <button class="btn btn-dark mt-2">Kelly Collection</button>
       </div>
     </div>
   </div>
-  <div class="container p-5 position-relative">
-    <div class="row justify-content-center align-items-center">
-      <div class="col-6 col-md-5 bg-white p-2 text-center">
+  <div class="container p-3 position-relative">
+    <div class="row align-items-center">
+      <div class="col-3 bg-white p-2 text-center">
         <h1>The Von Collection.</h1>
         <h4>Releasing Internationally.</h4>
         <h6>Raffle Entry Only.</h6>
@@ -34,7 +19,7 @@
           Find Out More.
         </RouterLink>
       </div>
-      <div class="col-6 col-xl-5 col-lg-5 col-md-5 col-sm-4 p-2">
+      <div class="col-3 p-2">
         <RouterLink to="/collection/von">
           <img
             src="../assets/images/collections/thumbnails/von-collection-1.jpg"
@@ -43,7 +28,7 @@
           />
         </RouterLink>
       </div>
-      <div class="col-6 col-xl-5 col-lg-5 col-md-5 col-sm-4 p-2">
+       <div class="col-3 p-2">
         <RouterLink to="/collection/von">
           <img
             src="../assets/images/collections/thumbnails/von-collection-2.jpg"
@@ -52,7 +37,7 @@
           />
         </RouterLink>
       </div>
-      <div class="col-6 col-xl-5 col-lg-5 col-md-5 col-sm-6 p-2">
+      <div class="col-3 p-2">
         <RouterLink to="/collection/von">
           <img
             src="../assets/images/collections/thumbnails/von-collection-3.jpg"
@@ -63,7 +48,7 @@
       </div>
     </div>
   </div>
-
+  
   <div class="container">
     <div class="row d-flex justify-content-center">
       <div class="col-12 text-center">
@@ -95,6 +80,25 @@
   object-fit: cover;
   width: 100%;
   height: 100%;
+}
+
+.hero-video {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  object-fit: cover;
+}
+
+.hero-container {
+  position: relative;
+  height: 1000px;
+}
+
+.hero-content {
+  z-index: 100;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
 }
 </style>
 
